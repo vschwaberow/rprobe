ecd5f6efd28d55a6692f833524d784bbcecfdb1b
Volker Schwaberow <volker@schwaberow.de>
2024-09-04 06:10:08 +0200

    chore: Improved plugin handler

--- src/plugins/apachebasic.rs
+++ src/plugins/apachebasic.rs
@@ -5,78 +5,27 @@
 
 use crate::httpinner::HttpInner;
 use crate::plugins::Plugin;
-use std::collections::HashMap;
 
-pub struct ApacheBasicPlugin {}
+pub struct ApacheBasicPlugin;
 
 impl Plugin for ApacheBasicPlugin {
-    fn name(&self) -> String {
-        "apache_basic".to_string()
+    fn name(&self) -> &'static str {
+        "Apache Basic"
     }
 
-    fn description(&self) -> String {
-        "This plugin detects basic Apache web servers".to_string()
-    }
-
-    fn version(&self) -> String {
-        "0.1.0".to_string()
-    }
-
-    fn author(&self) -> String {
-        "Volker Schwaberow".to_string()
-    }
-
-    fn license(&self) -> String {
-        "MIT".to_string()
-    }
-
-    fn run(&self, http_inner: &HttpInner) -> String {
-        let mut report_string: String = String::new();
-        let sig = self.add_signatures();
-        let mut found: Vec<String> = Vec::new();
-
-        let value = sig.get("APACHE_BASIC").unwrap();
-        value.iter().for_each(|x| {
-            if http_inner.body().contains(x) {
-                found.push("APACHE_BASIC".to_string());
-            }
-        });
-
-        found.iter().for_each(|x| {
-            if x.as_str() == "APACHE_BASIC" {
-                if found.contains(&"APACHE_BASIC".to_string()) {
-                    let s = "Apache Server".to_string();
-                    report_string += &s;
-                }
-                found.iter().for_each(|x| {
-                    if x.as_str() == "APACHE_BASIC" && found.contains(&"APACHE_BASIC".to_string()) {
-                        let s = "Apache Server".to_string();
-                        report_string += &s;
-                    }
-                });
-            }
-        });
-
-        report_string
-    }
-}
-
-impl ApacheBasicPlugin {
-    pub fn add_signatures(&self) -> HashMap<String, Vec<&str>> {
-        let mut sig = HashMap::new();
-
-        let sig_vector = vec![
+    fn run(&self, http_inner: &HttpInner) -> Option<String> {
+        let signatures = [
             "<html><body><h1>It works!</h1></body></html>",
             "<html>Apache is functioning normally</html>",
-            "<body><center>This IP is being shared among many domains.<br>\nTo view the domain you are looking for, simply enter the domain name in the location bar of your web browser.<br>",
+            "<body><center>This IP is being shared among many domains.<br>",
             "<html><head><title>Apache2 Ubuntu Default Page: It works</title></head>",
             "This IP is being shared among many domains.",
         ];
-        sig.insert("APACHE_BASIC".to_string(), sig_vector);
-        sig
-    }
 
-    pub fn new() -> Self {
-        Self {}
+        if signatures.iter().any(|&sig| http_inner.body().contains(sig)) {
+            Some("Apache Server detected".to_string())
+        } else {
+            None
+        }
     }
 }
--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -9,12 +9,8 @@ pub mod apachebasic;
 use crate::httpinner::HttpInner;
 
 pub trait Plugin {
-    fn name(&self) -> String;
-    fn description(&self) -> String;
-    fn version(&self) -> String;
-    fn author(&self) -> String;
-    fn license(&self) -> String;
-    fn run(&self, http_inner: &HttpInner) -> String;
+    fn name(&self) -> &'static str;
+    fn run(&self, http_inner: &HttpInner) -> Option<String>;
 }
 
 pub struct PluginHandler {
@@ -23,41 +19,29 @@ pub struct PluginHandler {
 
 impl PluginHandler {
     pub fn new() -> Self {
-        let mut phandler = Self {
+        let mut handler = Self {
             plugins: Vec::new(),
         };
-        phandler.register_known_plugins();
-        phandler
+        handler.register_known_plugins();
+        handler
     }
 
-    pub fn register(&mut self, plugin: Box<dyn Plugin>) {
-        self.plugins.push(plugin);
+    pub fn run(&self, http_inner: &HttpInner) -> Vec<String> {
+        self.plugins
+            .iter()
+            .filter_map(|plugin| {
+                plugin
+                    .run(http_inner)
+                    .map(|result| format!("{}: {}", plugin.name(), result))
+            })
+            .collect()
     }
 
     pub fn list(&self) -> Vec<String> {
-        let mut list = Vec::new();
-        for plugin in &self.plugins {
-            list.push(format!(
-                "{} {} - {}",
-                plugin.name(),
-                plugin.version(),
-                plugin.description()
-            ));
-        }
-        list.sort();
-        list
+        vec!["apachebasic".to_string()]
     }
 
-    pub fn run(&self, http_inner: &HttpInner) -> String {
-        let mut complete = String::new();
-        for plugin in &self.plugins {
-            let output = plugin.run(http_inner);
-            complete.push_str(&format!("{} ", output));
-        }
-        complete
-    }
-
-    pub fn register_known_plugins(&mut self) {
-        self.register(Box::new(apachebasic::ApacheBasicPlugin::new()));
+    fn register_known_plugins(&mut self) {
+        self.plugins.push(Box::new(apachebasic::ApacheBasicPlugin));
     }
 }
