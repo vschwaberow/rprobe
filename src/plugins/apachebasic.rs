58d72ace2b482addaed86b0ccf0ccea8a4f7b7fd
Volker Schwaberow <volker@schwaberow.de>
2024-12-09 06:29:31 +0100

    Sync (#43)
    * Improve error handling and enhance plugin functionality (#42)
    
    * chore: Improve error handling of http reponses
    
    * feat: Enhance Apache server detection in plugin with regex patterns
    
    * feat: Add plugin option to CLI and simplify stdin line reading
    
    * fix: Correct SPDX identifier comment and update plugin listing method visibility
    
    * feat: Add regex and log dependencies to Cargo.toml and update Cargo.lock
    
    * Update rprobe version to 0.7.0 in Cargo.toml and Cargo.lock
    
    * Add GitHub Actions workflow for build and release process

--- src/plugins/apachebasic.rs
+++ src/plugins/apachebasic.rs
@@ -5,6 +5,9 @@
 
 use crate::httpinner::HttpInner;
 use crate::plugins::Plugin;
+use log::info;
+use regex::Regex;
+use regex::RegexBuilder;
 
 pub struct ApacheBasicPlugin;
 
@@ -14,18 +17,48 @@ impl Plugin for ApacheBasicPlugin {
     }
 
     fn run(&self, http_inner: &HttpInner) -> Option<String> {
-        let signatures = [
-            "<html><body><h1>It works!</h1></body></html>",
-            "<html>Apache is functioning normally</html>",
-            "<body><center>This IP is being shared among many domains.<br>",
-            "<html><head><title>Apache2 Ubuntu Default Page: It works</title></head>",
-            "This IP is being shared among many domains.",
+        let body_patterns = [
+            r"<html><body><h1>It works!</h1></body></html>",
+            r"<html>Apache is functioning normally</html>",
+            r"<body><center>This IP is being shared among many domains\.<br>",
+            r"<html><head><title>Apache2 Ubuntu Default Page: It works</title></head>",
+            r"This IP is being shared among many domains\.",
+            r"Apache\/\d+\.\d+\.\d+",
         ];
 
-        if signatures.iter().any(|&sig| http_inner.body().contains(sig)) {
-            Some("Apache Server detected".to_string())
-        } else {
-            None
+        let header_patterns = [r"^Apache$", r"^Apache/\d+\.\d+(\.\d+)?$"];
+
+        for pattern in &body_patterns {
+            let re = Regex::new(pattern).unwrap();
+            if re.is_match(http_inner.body()) {
+                info!("Apache server detected in body with pattern: {}", pattern);
+                return Some("Apache Server detected in body".to_string());
+            }
         }
+
+        if let Some(server_header_value) = http_inner.headers().get("Server") {
+            let server_value = server_header_value.to_str().unwrap_or("");
+            for pattern in &header_patterns {
+                let re = RegexBuilder::new(pattern)
+                    .case_insensitive(true)
+                    .build()
+                    .unwrap();
+                if re.is_match(server_value) {
+                    info!("Apache server detected in header with pattern: {}", pattern);
+                    return Some("Apache Server detected in header".to_string());
+                }
+            }
+        }
+
+        if let Some(server_header_value) = http_inner.headers().get("server") {
+            let server_value = server_header_value.to_str().unwrap_or("");
+            let server_value_lower = server_value.to_lowercase();
+            if server_value_lower.contains("apache") {
+                info!("Apache-Server im Header erkannt: {}", server_value);
+                return Some("Apache-Server im Header erkannt".to_string());
+            }
+        }
+
+        None
     }
 }
