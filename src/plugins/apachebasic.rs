95788119722df25b4b3cbe7c720d7c299787b111
Volker Schwaberow <volker@schwaberow.de>
2025-02-05 06:34:55 +0100

    feat: Refactor ApacheBasicPlugin to use Lazy for regex patterns and improve performance

--- src/plugins/apachebasic.rs
+++ src/plugins/apachebasic.rs
@@ -1,3 +1,4 @@
+// File: apachebasic.rs
 // SPDX-License-Identifier: MIT OR Apache-2.0
 //
 // Copyright (c) 2023
@@ -6,10 +7,59 @@
 use crate::httpinner::HttpInner;
 use crate::plugins::Plugin;
 use log::info;
-use regex::Regex;
-use regex::RegexBuilder;
+use once_cell::sync::Lazy;
+use regex::{Regex, RegexBuilder};
 
 pub struct ApacheBasicPlugin;
+
+static BODY_PATTERNS: Lazy<Vec<(Regex, &str)>> = Lazy::new(|| {
+    vec![
+        (
+            Regex::new(r"<html><body><h1>It works!</h1></body></html>").unwrap(),
+            "Standard HTML Body",
+        ),
+        (
+            Regex::new(r"<html>Apache is functioning normally</html>").unwrap(),
+            "Apache Functioning Message",
+        ),
+        (
+            Regex::new(r"<body><center>This IP is being shared among many domains\.<br>").unwrap(),
+            "Shared IP Notice",
+        ),
+        (
+            Regex::new(r"<html><head><title>Apache2 Ubuntu Default Page: It works</title></head>").unwrap(),
+            "Ubuntu Default Page",
+        ),
+        (
+            Regex::new(r"This IP is being shared among many domains\.").unwrap(),
+            "Shared IP Message",
+        ),
+        (Regex::new(r"Apache\/\d+\.\d+\.\d+").unwrap(), "Apache Version Info"),
+    ]
+});
+
+static HEADER_PATTERNS: Lazy<Vec<(Regex, &str)>> = Lazy::new(|| {
+    vec![
+        (
+            RegexBuilder::new(r"^Apache$")
+                .case_insensitive(true)
+                .build()
+                .unwrap(),
+            "Server Header",
+        ),
+        (
+            RegexBuilder::new(r"^Apache/\d+\.\d+(\.\d+)?$")
+                .case_insensitive(true)
+                .build()
+                .unwrap(),
+            "Server Version Header",
+        ),
+        (
+            Regex::new(r"(?i)apache").unwrap(),
+            "Server Header Contains 'Apache'",
+        ),
+    ]
+});
 
 impl Plugin for ApacheBasicPlugin {
     fn name(&self) -> &'static str {
@@ -17,53 +67,18 @@ impl Plugin for ApacheBasicPlugin {
     }
 
     fn run(&self, http_inner: &HttpInner) -> Option<String> {
-        let body_patterns = [
-            (
-                r"<html><body><h1>It works!</h1></body></html>",
-                "Standard HTML Body",
-            ),
-            (
-                r"<html>Apache is functioning normally</html>",
-                "Apache Functioning Message",
-            ),
-            (
-                r"<body><center>This IP is being shared among many domains\.<br>",
-                "Shared IP Notice",
-            ),
-            (
-                r"<html><head><title>Apache2 Ubuntu Default Page: It works</title></head>",
-                "Ubuntu Default Page",
-            ),
-            (
-                r"This IP is being shared among many domains\.",
-                "Shared IP Message",
-            ),
-            (r"Apache\/\d+\.\d+\.\d+", "Apache Version Info"),
-        ];
-
-        let header_patterns = [
-            (r"^Apache$", "Server Header"),
-            (r"^Apache/\d+\.\d+(\.\d+)?$", "Server Version Header"),
-            (r"(?i)apache", "Server Header Contains 'Apache'"),
-        ];
-
         let mut detections = Vec::new();
 
-        for (pattern, description) in &body_patterns {
-            let re = Regex::new(pattern).unwrap();
+        for (re, description) in BODY_PATTERNS.iter() {
             if re.is_match(http_inner.body()) {
                 info!("Apache detected in body: {}", description);
                 detections.push(*description);
             }
         }
 
-        for (pattern, description) in &header_patterns {
-            if let Some(server_header_value) = http_inner.headers().get("Server") {
-                let server_value = server_header_value.to_str().unwrap_or("");
-                let re = RegexBuilder::new(pattern)
-                    .case_insensitive(*pattern == "(?i)apache")
-                    .build()
-                    .unwrap();
+        if let Some(server_header_value) = http_inner.headers().get("Server") {
+            let server_value = server_header_value.to_str().unwrap_or("");
+            for (re, description) in HEADER_PATTERNS.iter() {
                 if re.is_match(server_value) {
                     info!("Apache detected in header: {}", description);
                     detections.push(*description);
@@ -85,11 +100,8 @@ impl Plugin for ApacheBasicPlugin {
             ];
 
             detections.sort_by_key(|det| order.iter().position(|&o| o == *det).unwrap_or(order.len()));
-
             detections.dedup();
-
             let detection_message = detections.join(", ");
-
             Some(format!("Apache Server Detected: {}", detection_message))
         } else {
             None
